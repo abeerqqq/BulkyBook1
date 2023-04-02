f0a7527d5ad00110f4345780068dbc26c6610a21
abeerqqq <abeer-198@hotmail.com>
2023-04-02 14:39:45 +0300

    update

--- BulkyBookWeb/Areas/Admin/Controllers/ProductsController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductsController.cs
@@ -59,6 +59,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             */
             else
             {
+                productVM.Product = _unitOfWork.Products.GetFirstOrDefault(u => u.Id == id);
                 return View(productVM);
             }
             
@@ -76,6 +77,21 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                     string fileName = Guid.NewGuid().ToString();
                     var uploads = Path.Combine(wwwRootPath, @"images\products");
                     var extension = Path.GetExtension(file.FileName);
+
+                   /*If we are updating a product we have to delete the old image and add a new one in the images/products folder
+                    * 1. Check if there's an imageURL in the DB
+                    * 2. Get the Image Path
+                    */
+                    if (obj.Product.ImageUrl != null) {
+                        
+                        var oldImagePath = Path.Combine(wwwRootPath,obj.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath)) { 
+                            System.IO.File.Delete(oldImagePath);
+                        }
+
+
+                    }
+                    
                     using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
                     {
                         file.CopyTo(fileStreams);
@@ -83,7 +99,14 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                     obj.Product.ImageUrl = @"\images\products\" + fileName + extension;
 
                 }
-                _unitOfWork.Products.Add(obj.Product);
+                if(obj.Product.Id == 0)
+                {
+                    _unitOfWork.Products.Add(obj.Product);
+                }
+                else
+                {
+                    _unitOfWork.Products.Update(obj.Product);
+                }
                 _unitOfWork.Save();
                 TempData["success"] = "Product Created Successfully!";
                 return RedirectToAction("Index");
